c34d64f644d043bb71b2f36a9fcf24b752808d0a
SATHISH9910 <sathishmungi@gmail.com>
2026-07-20 23:04:17 +0530

    Update Find_1st_occurance.py

--- Find_1st_occurance.py
+++ Find_1st_occurance.py
@@ -11,14 +11,4 @@ if not found:            # runs only if no match was found
     print("Element not Found")
 
 
-    arr=[23,45,67,34,2]
-target=23
-found=False           # i =index
-
-for i in range(len(arr)-1):      # loop through all indices[all elements returns]
-    if arr[i]==target:       # compare the value not the index
-        print("First occurance at index",i)
-        found=True
-        break
-if not found:            # runs only if no match was found
-    print("Element not Found")
+  
